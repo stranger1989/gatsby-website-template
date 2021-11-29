06bd2f5556b966606bf79637c40e47430e5377d3
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2021-11-29 19:17:21 +0900

    Docs: Add JSDoc to functions

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -2,6 +2,12 @@ declare const window: Window['window'] & {
   dataLayer: Array<Record<string, unknown>>;
 };
 
+/**
+ * dispatch gtm trigger
+ * @module analytics
+ * @param {string} identifier - gtm identifier
+ * @return {null}
+ */
 export const gtmTrigger = (identifier: string) =>
   window.dataLayer.push({
     event: identifier,
